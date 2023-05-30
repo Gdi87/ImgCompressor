878c462f9ef645e533869808b9faa4e8a606f80a
AGB <129738915+Gdi87@users.noreply.github.com>
2023-05-30 15:06:54 +0200

    Update Compressor.py
    update progression bar

--- Compressor.py
+++ Compressor.py
@@ -1,13 +1,22 @@
-from PIL import Image # python3 -m pip install Pillow
-
+           
+from PIL import Image
 import os
+from tqdm import tqdm  # python3 -m pip install tqdm
 
 downloadsFolder = "/Users/Gdi87/Downloads/"
 
 if __name__ == "__main__":
+    image_extensions = [".jpg", ".jpeg", ".png"]
+    images_to_compress = []
+
+    #images list
     for filename in os.listdir(downloadsFolder):
         name, extension = os.path.splitext(downloadsFolder + filename)
+        if extension in image_extensions:
+            images_to_compress.append(filename)
 
-        if extension in [".jpg", ".jpeg", ".png"]:
-            picture = Image.open(downloadsFolder + filename)
-            picture.save(downloadsFolder + "compressed_"+filename, optimize=True, quality=66)
+    # compress and progress bar
+    for filename in tqdm(images_to_compress, desc="Compressing images"):
+        picture = Image.open(downloadsFolder + filename)
+        picture.save(downloadsFolder + "compressed_" + filename, optimize=True, quality=60)
+            
